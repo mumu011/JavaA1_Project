e50744db6abf7bf59050060429efd584a6a544d4
mumu <862136032@qq.com>
2021-11-28 18:33:12 +0800

    稳定运行版1

--- src/WuZiQi/timer_aike.java
+++ src/WuZiQi/timer_aike.java
@@ -12,9 +12,9 @@ public class timer_aike extends TimerTask {
         m_board = board;
         m_textfield = textField;
         // 每隔一分钟随机落子
-        //remain_time = 60;
+        remain_time = 60;
         // 测试
-        remain_time = 10;
+//        remain_time = 10;
         m_textfield.setText(String.valueOf(remain_time) + "s");
     }
 
@@ -24,8 +24,8 @@ public class timer_aike extends TimerTask {
         m_textfield.setText(String.valueOf(remain_time) + "s");
         if (remain_time == 0) {
             m_board.Random_luozi();
-//            remain_time = 60;
-            remain_time = 10;
+            remain_time = 60;
+//            remain_time = 10;
         }
     }
 }
